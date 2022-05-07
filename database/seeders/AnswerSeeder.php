b98fd6eff8fbbc38eae9a43735759289a4dee3a6
Kevin.Q <k.queyroi@gmail.com>
2022-05-07 12:57:54 +0200

    update answer_seeder

--- database/seeders/AnswerSeeder.php
+++ database/seeders/AnswerSeeder.php
@@ -16,11 +16,7 @@ class AnswerSeeder extends Seeder
      */
     public function run()
     {
-<<<<<<< HEAD
     //     $answerer=Answerer::where('access_token')->get();
-=======
-        // $answerer=Answerer::where('access_token')->get();
->>>>>>> seeder_answer
     //     $answerers = Answerer::all();
     //     $questions = Question::all();
     //     foreach ($answerers as $answerer) {
@@ -32,11 +28,7 @@ class AnswerSeeder extends Seeder
     //                     $newAnswer = Answer::create([
     //                         'description' => $description,
     //                         'question_id' => $question->id,
-<<<<<<< HEAD
     //                         'access_token'=> $answerer ->acces_token,
-=======
-    //                         'access_token'=> $answerer->acces_token,
->>>>>>> seeder_answer
     //                     ]);
     //                     break;
     //                 case 'B':
@@ -44,11 +36,7 @@ class AnswerSeeder extends Seeder
     //                     $newAnswer = Answer::create([
     //                         'description' => $randomAnswer,
     //                         'question_id' => $question->id,
-<<<<<<< HEAD
     //                         'access_token'=> $answerer ->acces_token,
-=======
-    //                         'access_token'=> $answerer->acces_token,
->>>>>>> seeder_answer
     //                     ]);
     //                     break;
     //                 case 'C':
@@ -56,11 +44,7 @@ class AnswerSeeder extends Seeder
     //                     $newAnswer = Answer::create([
     //                         'description' => $randomNumber,
     //                         'question_id' => $question->id,
-<<<<<<< HEAD
     //                         'access_token'=> $answerer ->acces_token,
-=======
-    //                         'access_token'=> $answerer->acces_token,
->>>>>>> seeder_answer
     //                     ]);
     //                     break;
     //                 default:
@@ -69,12 +53,7 @@ class AnswerSeeder extends Seeder
     //             $newAnswer->save();
     //         }   
     //     }
-<<<<<<< HEAD
-    // $a->status = true;
-    // $quiz->save();
-=======
-    // $answerer->status = true;
+    // $aanswerer->status = true;
     // $answerer->save();
->>>>>>> seeder_answer
     }
 } 
