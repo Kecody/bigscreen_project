d3a8088948a16a09f82f2187c69bf40c25105033
Kevin.Q <k.queyroi@gmail.com>
2022-05-20 09:16:21 +0200

    wepack changed

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
+mix.browserSync({proxy: "http://127.0.0.1:8000"})
+
+
 mix.js('resources/js/app.js', 'public/js','resources/js/bootstrap.js')
     .vue()
     .sass('resources/sass/app.scss', 'public/css');
